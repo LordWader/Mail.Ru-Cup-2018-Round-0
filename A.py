b9f1b5fc699928e5b82a94549b5c8e4ac833b785
Nick Proshin <40608791+LordWader@users.noreply.github.com>
2018-10-31 22:06:36 +0300

    Update A.py

--- A.py
+++ A.py
@@ -1,3 +1,17 @@
+"""
+Once upon a time there was only one router in the well-known company Bmail. Years went by and over time new routers were purchased. 
+Every time they bought a new router, they connected it to one of the routers bought before it. 
+You are given the values pi — the index of the router to which the i-th router was connected after being purchased (pi<i).
+There are nn routers in Boogle in total now. Print the sequence of routers on the path from the first to the nn-th router.
+Input
+The first line contains integer number n (2≤n≤200000) — the number of the routers. 
+The following line contains n−1 integers p2,p3,…,pn (1≤pi<i), where pi is equal to index of the router to 
+which the i-th was connected after purchase.
+Output
+Print the path from the 1-st to the n-th router. It starts with 1 and ends with n. 
+All the elements in the path should be distinct.
+"""
+
 import sys
 from collections import deque
 input = sys.stdin.readline
